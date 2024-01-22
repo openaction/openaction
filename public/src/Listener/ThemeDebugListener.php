b09445522778097607c6c2d079cebd306152d97f
Titouan Galopin <titouan@selency.com>
2024-01-22 20:14:10 +0100

    Display templating errors

---
+++ public/src/Listener/ThemeDebugListener.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Listener;
+
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Event\ExceptionEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+use Twig\Error\Error;
+
+class ThemeDebugListener implements EventSubscriberInterface
+{
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            KernelEvents::EXCEPTION => 'onKernelException',
+        ];
+    }
+
+    public function onKernelException(ExceptionEvent $event)
+    {
+        $e = $event->getThrowable();
+
+        if ($e instanceof Error) {
+            $event->setResponse(new Response(
+                content: 'Template error: '.$e->getMessage(),
+                headers: ['Content-Type' => 'text/plain'],
+            ));
+        }
+    }
+}
