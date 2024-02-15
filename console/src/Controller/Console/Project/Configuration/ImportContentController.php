b65234496f60f458df6181da9717e96fb2ea93da
István Pokorny <istvan.pokorny@crystalcode.eu>
2024-02-15 13:27:02 +0200

    Issue-54: add content import settings page, add settings field to entity, add settings form, update translations

--- console/src/Controller/Console/Project/Configuration/ImportContentController.php
+++ console/src/Controller/Console/Project/Configuration/ImportContentController.php
@@ -6,32 +6,35 @@ use App\Bridge\Uploadcare\UploadcareInterface;
 use App\Community\ImportExport\ContentImporter;
 use App\Controller\AbstractController;
 use App\Entity\Community\ContentImport;
+use App\Entity\Community\Model\ContentImportSettings;
+use App\Form\Project\ContentImportType;
 use App\Platform\Permissions;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 #[Route('/console/project/{projectUuid}/configuration/content/import')]
 class ImportContentController extends AbstractController
 {
-    public function __construct(private UploadcareInterface $uploadcare, private ContentImporter $importer)
+    public function __construct(private UploadcareInterface $uploadCare, private ContentImporter $importer)
     {
     }
 
     #[Route('', name: 'console_project_configuration_content_import')]
-    public function start()
+    public function start(): Response
     {
         // TODO! change to right permission
         $this->denyAccessUnlessGranted(Permissions::ORGANIZATION_COMMUNITY_MANAGE, $this->getOrganization());
         $this->denyIfSubscriptionExpired();
 
         return $this->render('console/project/configuration/content_import/start.html.twig', [
-            'uploadKey' => $this->uploadcare->generateUploadKey(),
+            'uploadKey' => $this->uploadCare->generateUploadKey(),
         ]);
     }
 
     #[Route('/prepare', name: 'console_project_configuration_content_import_prepare')]
-    public function prepare(Request $request)
+    public function prepare(Request $request): JsonResponse
     {
         // TODO! Change to right permissions
         $this->denyAccessUnlessGranted(Permissions::ORGANIZATION_COMMUNITY_MANAGE, $this->getOrganization());
@@ -39,7 +42,7 @@ class ImportContentController extends AbstractController
         $this->denyUnlessValidCsrf($request);
 
         // Download file locally
-        $file = $this->uploadcare->downloadFile(
+        $file = $this->uploadCare->downloadFile(
             $request->query->get('fileUuid'),
             pathinfo($request->query->get('fileName'), PATHINFO_EXTENSION),
         );
@@ -48,8 +51,9 @@ class ImportContentController extends AbstractController
             throw $this->createNotFoundException();
         }
 
-        // Prepare import
-        $import = $this->importer->prepareImport($this->getOrganization(), $file);
+        // currently only WP content can be imported, so we set the import type accordingly
+        // this can be extended with a form to select the import type before the file is uploaded
+        $import = $this->importer->prepareImport($this->getOrganization(), $file, ContentImportSettings::IMPORT_SOURCE_WORDPRESS);
 
         // Provide redirect URL to JavaScript
         return new JsonResponse([
@@ -61,16 +65,26 @@ class ImportContentController extends AbstractController
     }
 
     #[Route('/{uuid}/settings', name: 'console_project_configuration_content_import_settings')]
-    public function settings(ContentImport $import, Request $request)
+    public function settings(ContentImport $import, Request $request): Response
     {
         // TODO! Change to right permissions
         $this->denyAccessUnlessGranted(Permissions::ORGANIZATION_COMMUNITY_MANAGE, $this->getOrganization());
         $this->denyIfSubscriptionExpired();
         $this->denyUnlessSameOrganization($import);
 
+        $data = ContentImportSettings::createFromImport($import);
+        $form = $this->createForm(ContentImportType::class, $data, ['import_source' => $import->getSource()]);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->importer->startImport($import, $form->getData());
+
+            // TODO! Redirect to next step
+        }
+
         return $this->render('console/project/configuration/content_import/settings.html.twig', [
             'import' => $import,
-            'form' => '',
+            'form' => $form,
         ]);
     }
 }
