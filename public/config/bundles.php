079893ff201016eec07d6a798bb63a3f35b5bdec
Titouan Galopin <titouan@selency.com>
2024-03-12 16:29:45 +0100

    Add debug bundle in public prod for theme debug

--- public/config/bundles.php
+++ public/config/bundles.php
@@ -8,4 +8,5 @@ return [
     Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
+    Symfony\Bundle\DebugBundle\DebugBundle::class => ['all' => true],
 ];
