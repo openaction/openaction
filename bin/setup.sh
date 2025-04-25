176599960822b93dc6d1e702ea59370f9b34fe91
Titouan Galopin <titouan.galopin@citipo.com>
2025-04-25 17:36:55 +0200

    Add setup file

---
+++ bin/setup.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+docker compose up -d
+
+cd console
+yarn install
+yarn build
+
+cd projects
+yarn install
+yarn build
+
+cd ../../public
+yarn install
+yarn build
+
+# Install dependencies
+docker compose exec console composer install
+docker compose exec public composer install
+
+# Populate database
+docker compose exec console bin/console d:d:d --force -n
+docker compose exec console bin/console d:d:c -n
+docker compose exec console bin/console d:m:m -n
+docker compose exec console bin/console d:f:l --group test -n
+
+# Populate the search engine index
+docker compose exec console bin/console app:search:index-crm -n
+
+# Load the domains cache in Redis for public to work
+docker compose exec console bin/console app:proxy:refresh-domains-cache -n
