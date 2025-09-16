0ecc1e502ee1a4dfffd1e7d1ce20870c7f2f5c9c
Titouan Galopin <titouan.galopin@citipo.com>
2025-09-16 10:40:24 +0200

    Enable parallel php-cs

--- public/.php-cs-fixer.dist.php
+++ public/.php-cs-fixer.dist.php
@@ -7,6 +7,7 @@ $finder = PhpCsFixer\Finder::create()
 
 return (new PhpCsFixer\Config())
     ->setFinder($finder)
+    ->setParallelConfig(PhpCsFixer\Runner\Parallel\ParallelConfigFactory::detect())
     ->setRules([
         '@Symfony' => true,
         'array_syntax' => ['syntax' => 'short'],
