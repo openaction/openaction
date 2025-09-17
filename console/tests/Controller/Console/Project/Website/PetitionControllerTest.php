733a495e9c629525ff5ed7341b9556173f147ace
Titouan Galopin <titouan@autonorm.ai>
2025-09-17 13:59:57 +0000

    test(petitions): add list/create tests and fix parent delete selection

--- console/tests/Controller/Console/Project/Website/PetitionControllerTest.php
+++ console/tests/Controller/Console/Project/Website/PetitionControllerTest.php
@@ -8,6 +8,32 @@ use App\Tests\WebTestCase;
 
 class PetitionControllerTest extends WebTestCase
 {
+    public function testList(): void
+    {
+        $client = static::createClient();
+        $this->authenticate($client);
+
+        $crawler = $client->request('GET', '/console/project/'.self::PROJECT_ACME_UUID.'/website/petitions');
+        $this->assertResponseIsSuccessful();
+        $this->assertGreaterThanOrEqual(1, $crawler->filter('.world-list-row')->count());
+    }
+
+    public function testCreate(): void
+    {
+        $client = static::createClient();
+        $this->authenticate($client);
+
+        $crawler = $client->request('GET', '/console/project/'.self::PROJECT_ACME_UUID.'/website/petitions');
+        $this->assertResponseIsSuccessful();
+
+        $link = $crawler->selectLink('Create petition');
+        $this->assertGreaterThan(0, $link->count());
+        $client->click($link->link());
+
+        $this->assertResponseStatusCodeSame(302);
+        $this->assertMatchesRegularExpression('~^/console/project/'.self::PROJECT_ACME_UUID.'/website/petitions/localized/[0-9a-zA-Z\-]+/edit$~', $client->getResponse()->headers->get('Location'));
+    }
+
     public function testDelete(): void
     {
         $client = static::createClient();
@@ -22,9 +48,9 @@ class PetitionControllerTest extends WebTestCase
         $countBefore = $repository->count(['project' => $project->getId()]);
         $this->assertGreaterThanOrEqual(1, $countBefore);
 
-        // Click the first Delete link
-        $link = $crawler->selectLink('Delete')->link();
-        $client->click($link);
+        // Click the parent Delete button (not localized delete)
+        $deleteButton = $crawler->filter('a.petitions-list-action-delete')->first();
+        $client->click($deleteButton->link());
         $this->assertResponseRedirects();
 
         $crawler = $client->followRedirect();
