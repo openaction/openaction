087c1e36f0a3542bc5d432ce5659ac15915b1940
Titouan Galopin <titouan@autonorm.ai>
2025-12-23 15:54:07 +0000

    feat(public): add health probe endpoint

---
+++ public/src/Controller/HealthController.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class HealthController extends AbstractController
+{
+    /**
+     * @Route("/health", name="health", methods={"GET"})
+     */
+    public function health(): Response
+    {
+        return new Response('OK', Response::HTTP_OK, [
+            'Content-Type' => 'text/plain; charset=UTF-8',
+            'X-Robots-Tag' => 'noindex',
+        ]);
+    }
+}
