82126e3c1198d89620b52f265302568b135ebeb8
Titouan Galopin <titouan.galopin@citipo.com>
2025-12-26 23:47:15 +0100

    Revert Cloudflare update

--- console/public/index.php
+++ console/public/index.php
@@ -2,17 +2,6 @@
 
 use App\Kernel;
 
-if (isset($_SERVER['HTTP_CF_VISITOR'])) {
-    $cfVisitor = json_decode($_SERVER['HTTP_CF_VISITOR'], true);
-
-    if (is_array($cfVisitor) && ($cfVisitor['scheme'] ?? null) === 'https') {
-        // Force HTTPS semantics for downstream
-        $_SERVER['HTTP_X_FORWARDED_PROTO'] = 'https';
-        $_SERVER['HTTP_X_FORWARDED_PORT'] = '443';
-        $_SERVER['HTTP_X_FORWARDED_FOR'] = '127.0.0.1';
-    }
-}
-
 if (str_starts_with($_SERVER['REQUEST_URI'], '/projects/event')) {
     require __DIR__.'/../analytics/index.php';
     exit;
