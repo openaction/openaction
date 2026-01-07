3ad2138b342920ade8453046dbdabd4d8b3cc7aa
Titouan Galopin <titouan.galopin@citipo.com>
2026-01-07 14:16:57 +0100

    Don't link pod health to database health

--- console/src/Controller/HealthController.php
+++ console/src/Controller/HealthController.php
@@ -2,16 +2,11 @@
 
 namespace App\Controller;
 
-use Doctrine\DBAL\Connection;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class HealthController extends AbstractController
 {
-    public function __construct(private readonly Connection $db)
-    {
-    }
-
     #[Route('/health', name: 'health')]
     public function health(): Response
     {
@@ -19,22 +14,6 @@ class HealthController extends AbstractController
         $response->headers->set('Content-Type', 'text/plain');
         $response->headers->set('X-Robots-Tag', 'noindex');
 
-        if (!$this->isDatabaseHealthy()) {
-            $response->setStatusCode(Response::HTTP_INTERNAL_SERVER_ERROR);
-            $response->setContent('KO');
-        } else {
-            $response->setContent('OK');
-        }
-
         return $response;
-    }
-
-    private function isDatabaseHealthy(): bool
-    {
-        try {
-            return false !== stripos($this->db->query('SELECT version()')->fetchOne(), 'PostgreSQL');
-        } catch (\Throwable) {
-            return false;
-        }
     }
 }
