8bef437547ef25b710128ee14803d77af337c934
Titouan Galopin <titouan.galopin@citipo.com>
2026-01-28 18:21:50 +0100

    WIP

--- public/src/Form/Member/Model/UpdateAccountData.php
+++ public/src/Form/Member/Model/UpdateAccountData.php
@@ -26,7 +26,13 @@ class UpdateAccountData
     #[Assert\LessThanOrEqual(value: '15 years ago', message: '')]
     public ?\DateTime $profileBirthdate = null;
 
-    #[Assert\Choice(choices: ['', ''])]
+    #[Assert\Choice(choices: [
+        '',
+        'membership.join.choices.profileGender.male',
+        'membership.join.choices.profileGender.female',
+        'membership.join.choices.profileGender.non_binary',
+        'membership.join.choices.profileGender.other',
+    ])]
     public ?string $profileGender = null;
 
     #[Assert\Length(max: 50)]
